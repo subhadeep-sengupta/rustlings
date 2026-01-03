155b05baede9257d179394c027b7b2613dad8252
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2026-01-03 22:34:11 +0530

    3rd jan

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,7 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        //
     }
 }
 
