c35fba823300214730e6dc347def71f31680aece
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2026-01-04 22:09:44 +0530

    4th jan

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,19 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut new_string = String::from(input);
+    new_string.push_str(" world!");
+    new_string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    //
+    input.replace("cars", "balloons")
 }
 
 fn main() {
--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        match fruit {
+            Fruit::Mango => basket.insert(Fruit::Mango, 2),
+            Fruit::Apple => basket.insert(Fruit::Apple, 4),
+            Fruit::Lychee => basket.insert(Fruit::Lychee, 5),
+            Fruit::Banana => basket.insert(Fruit::Banana, 6),
+            Fruit::Pineapple => basket.insert(Fruit::Pineapple, 2),
+        };
     }
 }
 
